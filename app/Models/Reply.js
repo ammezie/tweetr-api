f236e0ac89ce41b82f9e21a813cec8071ee82eb4
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-26 22:31:23 +0100

    tweet and replies relationship

--- app/Models/Reply.js
+++ app/Models/Reply.js
@@ -3,6 +3,16 @@
 const Model = use('Model')
 
 class Reply extends Model {
+  /**
+   * A reply is to a tweet.
+   *
+   * @method tweet
+   *
+   * @return {Object}
+   */
+  tweet () {
+    return this.belongsTo('App/Models/Tweet')
+  }
 }
 
 module.exports = Reply
--- app/Models/Tweet.js
+++ app/Models/Tweet.js
@@ -13,6 +13,17 @@ class Tweet extends Model {
   user () {
     return this.belongsTo('App/Models/User')
   }
+
+  /**
+   * A tweet can have as many replies as possible.
+   *
+   * @method replies
+   *
+   * @return {Object}
+   */
+  replies () {
+    return this.hasMany('App/Models/Reply')
+  }
 }
 
 module.exports = Tweet
