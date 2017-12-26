8b47ace0e9e41cdfa0ea7d2dc2518eac5a1646cf
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-26 22:03:18 +0100

    user model and migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,9 +6,13 @@ class UserSchema extends Schema {
   up () {
     this.create('users', table => {
       table.increments()
+      table.string('name').notNullable()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('location').nullable()
+      table.string('website_url').nullable()
+      table.text('bio').nullable()
       table.timestamps()
     })
   }
