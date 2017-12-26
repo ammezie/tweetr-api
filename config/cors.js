7a65d1c8c180b0708906a3b0324995f658d3ec2b
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-26 21:53:53 +0100

    enabling CORS

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: false,
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ module.exports = {
   | Array - An array of allowed methods
   |
   */
-  methods: ['GET', 'PUT', 'POST'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
 
   /*
   |--------------------------------------------------------------------------
